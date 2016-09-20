50f73e368202e130b50c415e65a42cc209c49c34
Rakesh Yadav <rakesh.yadav@limetray.com>
2016-09-20 18:40:00 +0530

    model id included in file path

--- lib/biz_webp/webpconverter.rb
+++ lib/biz_webp/webpconverter.rb
@@ -3,44 +3,31 @@
 require 'webp-ffi'
 require 'carrierwave/sanitized_file'
 module CarrierWave
-  module BizWebp
-    module Webpconverter extend CarrierWave::MiniMagick
-      def convert_to_webp(options = {},path)
-        manipulate! do |img|
-          img          = yield(img) if block_given?
-          puts path
-          webp_path    = path+"#{img.path}.webp"
-          old_filename = filename
+module BizWebp
+  module Webpconverter
+    def convert_to_webp(options = {})
+      manipulate! do |img|
+        img          = yield(img) if block_given?
+        webp_path    = "#{img.path}.webp"
+        old_filename = filename
 
-          puts "image current_path"
+        ::WebP.encode(img.path, webp_path, options)
 
-          puts current_path
+        # XXX: Hacks ahead!
+        # I can't find any other way to store an alomost exact copy
+        # of file for any particular version
+        instance_variable_set('@filename', "#{filename}.webp")
 
-          ::WebP.encode(img.path, webp_path, options)
+        storage.store! SanitizedFile.new({
+                                             tempfile: webp_path, filename: current_path+webp_path,
+                                             content_type: 'image/webp'
+                                         })
 
-          puts "hey--->"
-          puts webp_path.to_s()
+        instance_variable_set('@filename', old_filename)
 
-          puts "filename--->"
-          puts old_filename
-
-          puts "newfilename"
-
-          # XXX: Hacks ahead!
-          # I can't find any other way to store an alomost exact copy
-          # of file for any particular version
-          #instance_variable_set('@filename', "#{filename}.webp")
-
-          storage.store! SanitizedFile.new({
-                                               tempfile: webp_path, filename: webp_path,
-                                               content_type: 'image/webp'
-                                           })
-
-          instance_variable_set('@filename', old_filename)
-
-          ::MiniMagick::Image.open(webp_path)
-        end
+        img
       end
     end
   end
+end
 end
